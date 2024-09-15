dfc8ed44a6ff72cc714c6246127625c2394b0d98
Wong <josuewong797@gmail.com>
2024-09-15 12:22:56 -0600

    Se elimina clase Patient

--- Lab2/AEDLaboratorio2/AEDLaboratorio2/Entities/DentalClinic/Appointment.cs
+++ Lab2/AEDLaboratorio2/AEDLaboratorio2/Entities/DentalClinic/Appointment.cs
@@ -3,7 +3,8 @@
     public class Appointment
     {
         public int Id { get; set; }
-        public Patient? Patient { get; set; }
+        public string? PatientName { get; set; }
+        public string? PatientSurname { get; set; }
         public string? Service { get; set; }
         public DateTime ScheduledDate { get; set; }
     }
